48fec1301e145e095326e1d45cb2ab04c0c7283b
yaoxin <271958091@qq.com>
2022-02-09 17:59:45 +0800

    15

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,9 +1,18 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
+		<text selectable space="nbsp" :decode="false">须知少时凌云志，曾许人间第一流</text>
+    <view class="">{{title}}</view>
+    <view class="div" hover-class="hover">
+      <view
+        class="div1"
+        hover-stop-propagation
+        hover-start-time="1000"
+        hover-stay-time="500"
+        hover-class="hover-div1"></view>
+    </view>
+    <button type="primary" v-on:click="clickBtn">点击</button>
+    <image src="../../static/a.jpg" mode="widthFix"></image>
+    
 	</view>
 </template>
 
@@ -11,42 +20,41 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				title: 'Hello',
+        dis: true
 			}
 		},
 		onLoad() {
 
 		},
 		methods: {
-
+      clickBtn () {
+        console.log(1111)
+      }
 		}
 	}
 </script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
+<style lang="scss">
+  .div {
+    width: 200rpx;
+    height: 200rpx;
+    border: 1rpx solid #4CD964;
+  }
+  .div1 {
+    width: 100rpx;
+    height: 100rpx;
+    background: #333333;
+  }
+  .hover {
+    background-color: #808080;
+  }
+  .hover-div1 {
+    background-color: #C0C0C0;
+  }
+  text {
+    color: $uni-color-primary;
+  }
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+	
 </style>
